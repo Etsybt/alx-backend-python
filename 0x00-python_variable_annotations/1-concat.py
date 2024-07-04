dd197fb24d03325fb14ff148fe371f308525fb19
Etsybt <nouhailahmidouch2@gmail.com>
2024-07-04 12:59:12 +0000

    update

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+concatenated string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    type-annotated function, returns concatenated str
+    """
+    return str1 + str2
