75b37bdea6828ed96ace1c9cd7fa8dd729202b65
Etsybt <nouhailahmidouch2@gmail.com>
2024-07-04 13:08:28 +0000

    update

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+floor of a float
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """
+     type-annotated function, return floor of a float
+    """
+    return (math.floor(n))
