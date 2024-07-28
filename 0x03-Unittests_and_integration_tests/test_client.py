b5a2fc7bebce528bbe99a97b1b1dc285eab64583
Etsybt <nouhailahmidouch2@gmail.com>
2024-07-28 14:04:43 +0000

    update

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -31,6 +31,9 @@ class TestGithubOrgClient(unittest.TestCase):
 
     @patch('client.GithubOrgClient.org', new_callable=PropertyMock)
     def test_public_repos_url(self, mock_org):
+        """
+        test method
+        """
         mock_org.return_value = {
             "repos_url": "https://api.github.com/orgs/google/repos"}
         client = GithubOrgClient("google")
@@ -39,6 +42,9 @@ class TestGithubOrgClient(unittest.TestCase):
 
     @patch('client.get_json')
     def test_public_repos(self, mock_get_json):
+        """
+        test method
+        """
         mock_get_json.return_value = [
             {"name": "repo1"},
             {"name": "repo2"}
@@ -47,7 +53,7 @@ class TestGithubOrgClient(unittest.TestCase):
                 'client.GithubOrgClient._public_repos_url',
                 new_callable=PropertyMock
         ) as mock_public_repos_url:
-            mock_public_repos_url.return_value ="https://api.github.com/orgs/google/repos"
+            mock_public_repos_url.return_value = "https://api.github.com/orgs/google/repos"
             client = GithubOrgClient("google")
             result = client.public_repos()
             expected_result = ["repo1", "repo2"]
@@ -56,6 +62,17 @@ class TestGithubOrgClient(unittest.TestCase):
             mock_public_repos_url.assert_called_once()
             mock_get_json.assert_called_once_with(
                 "https://api.github.com/orgs/google/repos")
+
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False)
+    ])
+    def test_has_license(self, repo, license_key, expected):
+        """
+        test method
+        """
+        result = GithubOrgClient.has_license(repo, license_key)
+        self.assertEqual(result, expected)
 
 
 if __name__ == '__main__':
