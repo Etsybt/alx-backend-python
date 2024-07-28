969df863ad83731cf1858a60060348c3e229a11c
Etsybt <nouhailahmidouch2@gmail.com>
2024-07-28 14:00:50 +0000

    update

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -26,16 +26,36 @@ class TestGithubOrgClient(unittest.TestCase):
         client = GithubOrgClient(org_name)
         result = client.org
         mock_get_json.assert_called_once_with(
-                f"https://api.github.com/orgs/{org_name}")
+            f"https://api.github.com/orgs/{org_name}")
         self.assertEqual(result, {"org_name": org_name})
 
     @patch('client.GithubOrgClient.org', new_callable=PropertyMock)
     def test_public_repos_url(self, mock_org):
         mock_org.return_value = {
-                "repos_url": "https://api.github.com/orgs/google/repos"}
+            "repos_url": "https://api.github.com/orgs/google/repos"}
         client = GithubOrgClient("google")
         result = client._public_repos_url
         self.assertEqual(result, "https://api.github.com/orgs/google/repos")
+
+    @patch('client.get_json')
+    def test_public_repos(self, mock_get_json):
+        mock_get_json.return_value = [
+            {"name": "repo1"},
+            {"name": "repo2"}
+        ]
+        with patch(
+                'client.GithubOrgClient._public_repos_url',
+                new_callable=PropertyMock
+        ) as mock_public_repos_url:
+            mock_public_repos_url.return_value ="https://api.github.com/orgs/google/repos"
+            client = GithubOrgClient("google")
+            result = client.public_repos()
+            expected_result = ["repo1", "repo2"]
+
+            self.assertEqual(result, expected_result)
+            mock_public_repos_url.assert_called_once()
+            mock_get_json.assert_called_once_with(
+                "https://api.github.com/orgs/google/repos")
 
 
 if __name__ == '__main__':
